2545e61f24b60836d362e954f65260fb456ba788
Hlumelo <contacthlumelo@gmail.com>
2022-10-19 03:24:00 +0200

    added a label as heading

--- game.java
+++ game.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.Font;
 
 import javax.swing.*;
 
@@ -13,10 +14,30 @@ public class game {
     public void gui() {    
         JFrame screen = new JFrame(this.title);
 
-        screen.setSize(1000,800);
+        JLabel text1 = new JLabel();
+        text1.setVerticalAlignment(JLabel.TOP);
+        text1.setHorizontalAlignment(JLabel.CENTER);
+
+        text1.setText("Let's play Rock Paper Scissors !!!");
+        text1.setFont(new Font("Verdana", Font.PLAIN, 18));
+        
+
+        screen.setSize(1080,800);
         screen.setLocation(120, 60);
         screen.getContentPane().setBackground(Color.ORANGE);
+        screen.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        screen.add(text1);
         screen.setVisible(true);
+
+
+
+        int pScore = 0;
+        int cScore = 0;
+
+        while (true) {
+
+            
+        }
     }
     
 }
